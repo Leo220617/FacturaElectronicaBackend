470018bfd4c8370886df1db5073e72ab6d5752db
Leo220617 <32038650+Leo220617@users.noreply.github.com>
2022-08-25 16:09:37 -0600

    cambios

---
+++ Sicsoft.Checkin.Web/Models/AceptacionParametros.cs
@@ -0,0 +1,9 @@
+﻿namespace FacturaElectronica.Models
+{
+    public class AceptacionParametros
+    {
+        public string tipo { get; set; }
+        public string condicionImpuesto { get; set; }
+        public string AEconomica { get; set; }
+    }
+}
---
+++ Sicsoft.Checkin.Web/Pages/Aceptacion/AceptacionMasiva.cshtml.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Castle.Core.Configuration;
+using FacturaElectronica.Models;
+using InversionGloblalWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Refit;
+using Sicsoft.Checkin.Web.Servicios;
+
+namespace FacturaElectronica.Pages.Aceptacion
+{
+    public class AceptacionMasivaModel : PageModel
+    {
+        private readonly ICrudApi<BandejaEntradaViewModel, int> sBandeja;
+        private readonly ICrudApi<RecibidoRoles, int> compras;
+        private readonly ICrudApi<ParametrosViewModel, int> service;
+
+        [BindProperty]
+        public BandejaEntradaViewModel[] Bandejas { get; set; }
+
+        [BindProperty]
+        public ParametrosViewModel Parametros { get; set; }
+
+        [BindProperty]
+        public string Sucursal { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public ParametrosFiltros filtro { get; set; }
+
+        public AceptacionMasivaModel(ICrudApi<BandejaEntradaViewModel, int> sBandeja, ICrudApi<RecibidoRoles, int> compras, ICrudApi<ParametrosViewModel, int> service)
+        {
+            this.sBandeja = sBandeja;
+            this.compras = compras;
+            this.service = service;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            try
+            {
+
+
+                var Roles1 = ((ClaimsIdentity)User.Identity).Claims.Where(d => d.Type == "Roles").Select(s1 => s1.Value).FirstOrDefault().Split("|");
+                if (string.IsNullOrEmpty(Roles1.Where(a => a == "19").FirstOrDefault()))
+                {
+                    return RedirectToPage("/NoPermiso");
+                }
+
+
+                Parametros = await service.ObtenerPorId(1);
+
+                DateTime time = new DateTime();
+
+                if (time == filtro.FechaInicial)
+                {
+
+                    await compras.RealizarLecturaEmails();
+                    filtro.FechaInicial = DateTime.Now;
+
+                    filtro.FechaInicial = new DateTime(filtro.FechaInicial.Year, filtro.FechaInicial.Month, 1);
+
+
+                    DateTime primerDia = new DateTime(filtro.FechaInicial.Year, filtro.FechaInicial.Month, 1);
+
+
+                    DateTime ultimoDia = primerDia.AddMonths(1).AddDays(-1);
+
+                    filtro.FechaFinal = ultimoDia;
+
+
+
+                }
+
+                
+                Bandejas = await sBandeja.ObtenerLista(filtro);
+
+                Bandejas = Bandejas.Where(a => a.Procesado != "1").ToArray();
+
+                Sucursal = Bandejas.Where(a => !string.IsNullOrEmpty(a.CodigoActividad)).FirstOrDefault() == null ? "" : Bandejas.Where(a => !string.IsNullOrEmpty(a.CodigoActividad)).FirstOrDefault().CodigoActividad;
+
+                return Page();
+            }
+            catch (ApiException ex)
+            {
+                Errores error = JsonConvert.DeserializeObject<Errores>(ex.Content.ToString());
+                ModelState.AddModelError(string.Empty, error.Message);
+
+                return Page();
+            }
+        }
+
+
+        public async Task<IActionResult> OnPostGenerar(int[] recibidos, AceptacionParametros acep)
+        {
+            string error = "";
+
+
+            try
+            {
+                Bandejas = await sBandeja.ObtenerLista(filtro);
+                Bandejas = Bandejas.Where(a => a.Procesado != "1").ToArray();
+
+                for (int i = 0; i < recibidos.Length; i++)
+                {
+                    var Bandeja = Bandejas.Where(a => a.Id == recibidos[i]).FirstOrDefault();
+
+                    Bandeja.tipo = acep.tipo;
+                    switch (Bandeja.tipo)
+                    {
+                        case "05":
+                            {
+                                Bandeja.Mensaje = "1";
+                                break;
+                            }
+                        case "06":
+                            {
+                                Bandeja.Mensaje = "2";
+                                break;
+                            }
+                        case "07":
+                            {
+                                Bandeja.Mensaje = "3";
+                                break;
+                            }
+                    }
+                    Bandeja.DetalleMensaje = "Aceptacion Masiva";
+                    Bandeja.CondicionImpuesto = acep.condicionImpuesto;
+                    Bandeja.CodigoActividad = acep.AEconomica;
+                    Bandeja.impuestoAcreditar = Math.Round(Bandeja.Impuesto);
+                    Bandeja.gastoAplicable = Math.Round(Bandeja.TotalComprobante.Value);
+                    var a = await sBandeja.Agregar(Bandeja);
+                }
+
+                //await service.Agregar(recibidos);
+
+
+                return new JsonResult(true);
+            }
+            catch (ApiException ex)
+            {
+                Errores errores = JsonConvert.DeserializeObject<Errores>(ex.Content.ToString());
+                ModelState.AddModelError(string.Empty, errores.Message);
+                return new JsonResult(errores.Message);
+                //return new JsonResult(false);
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError(string.Empty, ex.Message);
+
+                return new JsonResult(false);
+            }
+        }
+
+    }
+}
